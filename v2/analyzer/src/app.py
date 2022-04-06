ac02ef0d2491ec914267c4e253de53d1d8c9d2c6
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2022-04-06 13:14:46 +0530

    Update app.py

--- v2/analyzer/src/app.py
+++ v2/analyzer/src/app.py
@@ -6,6 +6,7 @@ import time
 import logging
 from logging import StreamHandler
 from retry import retry
+import urllib.parse
 
 # Define the base logger
 logging.getLogger("analyzer").setLevel(logging.DEBUG)
@@ -21,7 +22,7 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 
 # Define global variables
 
-global tone_analyzer_ep
+global NLU_ep
 
 global identity_token_url
 
@@ -35,28 +36,28 @@ global is_refresh
 
 global expire_time
 
-def analyze_tone(input_text):
-    log.info("tone_analyzer_ep is: %s ", tone_analyzer_ep)
+def nlu(input_text):
+    log.info("NLU_ep is: %s ", NLU_ep)
 
     # before getting tone, check if token is expired and refresh if so.
     if datetime.utcnow() > expire_time:
         generate_tokens(is_refresh)
 
-    if access_token:
-	log.info("access token is there.")
-    else:
-	log.debug("access_token not existing")
-	return None
+    # if access_token:
+	#     log.info("access token is there.")
+	# else:
+	#      log.debug("access_token not existing")
+    # return None
 
     headers = {
          'Content-Type': 'text/plain',
      'Authorization': 'Bearer ' + access_token,
     }
 
-    r = requests.post(tone_analyzer_ep, headers=headers, data=input_text)
-    log.info("tone_analyzer_ep response code is: %s ", r.status_code)
+    r = requests.post(NLU_ep, headers=headers, data=input_text)
+    log.info("NLU_ep response code is: %s ", r.status_code)
     if r.status_code != 200:
-        log.error("FAILED analyze tone: '%s', msg: '%s'", input_text, r.text)
+        log.error("FAILED analyze sentence: '%s', msg: '%s'", input_text, r.text)
         return None
     log.info("return %s ", r.json())
     return r.json()
@@ -68,9 +69,9 @@ def analyze_tone(input_text):
      "input_text": "this is cool"
  }
 '''
-@app.route('/tone', methods=['POST'])
+@app.route('/nlu', methods=['POST'])
 def tone():
-    log.info("Serving /tone")
+    log.info("Serving /nlu")
     if not request.json or not 'input_text' in request.json:
         log.error("bad body: '%s'", request.data)
         abort(400)
@@ -78,9 +79,9 @@ def tone():
     input_text = request.json['input_text']
 
     log.info("input text is '%s'", input_text)
-    tone_doc = analyze_tone(input_text)
+    tone_doc = nlu(input_text)
 
-    return (json.dumps(tone_doc['document_tone']['tones']), 200)
+    return (json.dumps(tone_doc), 200)
 
 
 '''
@@ -89,7 +90,8 @@ POST identity/token method to generate an IAM access token by passing an API key
 @retry(Exception, delay=2, tries=5)
 def generate_tokens(refresh):
 
-	api_key = os.getenv('TONE_ANALYZER_API_KEY')
+	# api_key = os.getenv('NLU_API_KEY')
+	api_key="aI4yamCk4QY_kEckMh8zqv7IH2GmR2mJpj6-mNQ2OEvc"
 
 	params = None
 
@@ -106,7 +108,7 @@ def generate_tokens(refresh):
 	if api_key:
 		log.info("api key is there.")
 	else :
-		log.error("TONE_ANALYZER_API_KEY not set")
+		log.error("NLU_API_KEY not set")
 
 	if refresh:
 		headers = {
@@ -128,7 +130,7 @@ def generate_tokens(refresh):
 			'apikey': api_key,
 		}
 
-	params = urllib.urlencode(params)
+	params = urllib.parse.urlencode(params)
 
 	log.debug("calling identity token method : %s ", params)
 
@@ -167,13 +169,13 @@ if __name__ == '__main__':
 
     PORT = '5000'
 
-    api_url = os.getenv('TONE_ANALYZER_SERVICE_API')
+    api_url = "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/417e90df-01b7-4091-9b95-a04b1e2f37cf"
 
     if not api_url:
-        log.error("TONE_ANALYZER_SERVICE_API not set")
+        log.error("NLU_SERVICE_API not set")
 
-    # https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21&sentences=false
-    tone_analyzer_ep = "" + api_url + "/v3/tone?version=2017-09-21&sentences=false"
+    #  https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/417e90df-01b7-4091-9b95-a04b1e2f37cf/v1/analyze?version=2021-08-01&features=keywords,entities&entities.emotion=true&entities.sentiment=true&keywords.emotion=true&keywords.sentiment=true 
+    NLU_ep = "" + api_url + "/v1/analyze?version=2021-08-01&features=keywords,entities&entities.emotion=true&entities.sentiment=true&keywords.emotion=true&keywords.sentiment=true"
 
     identity_token_url = "https://iam.cloud.ibm.com/identity/token"
 
@@ -185,5 +187,5 @@ except Exception as err:
 	log.debug('SSL connection failed: %s', str(err))
 
 finally :
-	log.info("Starting analyzer tone_analyzer_ep: %s ", tone_analyzer_ep)
+	log.info("Starting analyzer NLU_ep: %s ", NLU_ep)
 	app.run(host='0.0.0.0', port=int(PORT))
