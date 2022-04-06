61c2f2ed10bed497ea2e40776aa46d58d8a986f2
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2022-04-06 16:38:48 +0530

    Update binding-hack.sh

--- v2/binding-hack.sh
+++ v2/binding-hack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tone_analyzer="Natural Language Understanding-if"
+tone_analyzer="Natural Language Understanding-dt"
 
 B64_URL="https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/922146de-0c00-4996-b902-790726956a6c"
 B64_APIKEY="LVg4xDiSWfdIOf69KXqYq47WIAONS9VZCXg9PZaC5GHZ"
