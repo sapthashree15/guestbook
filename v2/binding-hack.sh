006c7349e9ecf3283d26761339ddd9f1e573919c
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2022-04-07 10:55:51 +0530

    Update binding-hack.sh

--- v2/binding-hack.sh
+++ v2/binding-hack.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-B64_URL=$(ibmcloud resource service-key "Auto-generated service credentials" --output json | jq .[0].credentials.url -j | base64 -w 0)
-B64_APIKEY=$(ibmcloud resource service-key "Auto-generated service credentials" --output json | jq .[0].credentials.apikey -j | base64 -w 0)
+
+tone_analyzer="Natural Language Understanding-dt"
+
+B64_URL=$(ibmcloud resource service-keys --instance-name "$tone_analyzer" --output json| jq .[0].credentials.url -j | base64 -w 0)
+B64_APIKEY=$(ibmcloud resource service-keys --instance-name "$tone_analyzer" --output json| jq .[0].credentials.apikey -j | base64 -w 0)
+
 cat <<EOF | oc apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
   name: binding-tone
-  namespace: sn-labs-sapthashreek
+  namespace: <my_project>
 type: Opaque
 data:
   url: $B64_URL
